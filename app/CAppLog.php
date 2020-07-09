8e22c549938a63f09d6e007277572fb1cb3e0136
rururu3 <rururulily@gmail.com>
2020-07-10 00:51:59 +0900

    例外のログだし

--- app/CAppLog.php
+++ app/CAppLog.php
@@ -18,6 +18,7 @@ class CAppLog {
       'loggers' => [
         'develop' => [
           'level' => 'DEBUG',
+          'additivity' => false,
           'appenders' => ['default'],
         ],
       ],
